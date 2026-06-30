378e2ea9e8a12861aa4d64bada3786e34aaf797c
ELLOCO509 <metelusherline@gmail.com>
2026-06-30 07:48:40 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,9 +10,9 @@ export async function pingTest(client, message) {
 
     await client.sendMessage(remoteJid, {
         text: stylizedChar(
-            `🚀 Digital Crew Network\n\n` +
+            `🚀 EL LOCO BOT Network\n\n` +
             `Latency: ${latency} ms\n\n` +
-            `Digital Crew 243`
+            `EL LOCO`
         )
     }, { quoted: message })
 }
