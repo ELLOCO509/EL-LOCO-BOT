19d7230347fd35e17411ae579aedb1e8efa67ffd
ELLOCO509 <metelusherline@gmail.com>
2026-06-30 14:23:47 -0400

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -13,7 +13,7 @@ export async function uptime(client, message) {
 │ 💾 RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)}MB
 │
 │ "Beyond limits, we rise."
-│     - EL LOCO -
+│     - EL LOCO BOT-
 └────────────────────┘`
     
     await client.sendMessage(remoteJid, { text: text })
